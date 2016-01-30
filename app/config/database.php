afb6e85040e6840229a0b3cfd19cca290f0830ae
Morriel Aquino <bimbsaquino090409@gmail.com>
2016-01-31 07:25:43 +0800

    customerprofilecompany migration and model file

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'Tailoring',
+			'database'  => 'tailoring',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
---
+++ app/database/migrations/2016_01_30_212529_tblCustomerProfileCompanies.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TblCustomerProfileCompanies extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tblCustomerProfileCompanies', function(Blueprint $table){
+			$table->integer('strCustProfComID')->primary();
+			$table->string('strCustProfComName');
+			$table->string('strCustProfComAddress');
+			$table->string('strCustProfComDesc');
+			$table->string('strCustProfComEmail');
+			$table->string('strCustProfComCellNo');
+			$table->string('strCustProfComPhoneNo');
+			$table->string('strCustProfComFax');
+			$table->string('strCustProfComContactPerson');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('tblCustomerProfileCompanies');
+	}
+
+}
