2b1d2d5b8e7b2d9cf5267df51473b913f9baa924
Daniel <israelvalle48@gmail.com>
2016-02-01 15:35:42 +0800

    indention of master layout codes

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -6,37 +6,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     {{ HTML::style('css/materialize.min.css') }}
+	{{ HTML::style('css/style.css') }}
 
-    <style>
-    	header, main, footer {
-      	padding-left: 240px;
-    	}
-
-    	@media only screen and (max-width : 992px) {
-      		header, main, footer {
-        	padding-left: 0;
-      		}
-    	}
-    	
-    	a{
-    		color: lightcoral;
-    	}
-
-    	#shop{
-    		margin-right:20px;
-    	}
-
-    	nav{
-    		background-color: lavender;
-    	}
-
-    	.waves-effect.waves-light .waves-ripple {
-
-	    	background-color: lightcoral;
-    	}
-
-
-    </style>
 </head>
 
 <body>
@@ -45,108 +16,99 @@
 
 
 		<ul id="slide-out" class="side-nav fixed">
-			<li>
-				<div id="logo"></div>
-			</li>
-			<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Sales</a></li>
-			<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Job Order</a></li>
-			
-			<!--Maintenance Dropdown-->
-			<li class="no-padding">
-				<ul class="collapsible collapsible-accordion">
-					<li>
-						<a style="color: lightcoral" class="waves-effect waves-light btn-small, collapsible-header">Maintenance</a>
-						<div class="collapsible-body">
-							<ul>
-								<!--Customer Profile Dropdown-->
-								<li class="no-padding">
-									<ul class="collapsible collapsible-accordion">
-										<li>
-											<a style="color: lightcoral" class="waves-effect waves-light btn-small, collapsible-header">Customer Profile</a>
+		<li><div id="logo"></div></li>
+		<li><b><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Sales</a></b></li>
+		<li><b><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Job Order</a></b></li>
+		
+		<!--Maintenance Dropdown-->
+		<li class="no-padding">
+		<ul class="collapsible collapsible-accordion">
+			<li><a style="color: lightcoral" class="waves-effect waves-light btn-small, collapsible-header"><b>Maintenance</b></a>
+				<div class="collapsible-body">
+					<ul>
+					<!--Customer Profile Dropdown-->
+						<li class="no-padding">
+						<ul class="collapsible collapsible-accordion">
+							<li><a style="color: lightcoral" class="waves-effect waves-light btn-small, collapsible-header">Customer Profile</a>
+								<div class="collapsible-body">
+									<ul>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/customerIndividual">Individual</a></li>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/customerCompany">Company</a></li>
+									</ul>
+								</div>
+							</li>
+						</ul>
+						</li>
+					<!--Employee Dropdown-->			
+						<li class="no-padding">
+						<ul class="collapsible collapsible-accordion">
+							<li><a style="color: lightcoral" class="waves-effect waves-light btn-small, collapsible-header">Employee</a>
+								<div class="collapsible-body">
+									<ul>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/employee">Employee Profile</a></li>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/employeeRole">Position Roles</a></li>
+									</ul>
+								</div>
+							</li>
+						</ul>
+						</li>
+					<!--Garments Dropdown-->
+						<li class="no-padding">
+						<ul class="collapsible collapsible-accordion">
+							<li><a style="color: lightcoral" class="waves-effect waves-light btn-small, collapsible-header">Garments</a>
+								<div class="collapsible-body">
+									<ul>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/garments">Category</a></li>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/garmentsDetails">Details</a></li>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/designPattern">Design Pattern</a></li>
+									<!--Measurements Dropdown-->
+										<li class="no-padding">
+										<ul class="collapsible collapsible-accordion">
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small, collapsible-header">Measurements</a>
 											<div class="collapsible-body">
 												<ul>
-													<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/customerIndividual">Individual</a></li>
-													<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/customerCompany">Company</a></li>
+													<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/measurementCategory">Header</a></li>
+								  					<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/measurementDetails	">Details</a></li>
 												</ul>
 											</div>
 										</li>
-									</ul>
-								</li>
-								<!--Employee Dropdown-->			
-								<li class="no-padding">
-									<ul class="collapsible collapsible-accordion">
-										<li>
-											<a style="color: lightcoral" class="waves-effect waves-light btn-small, collapsible-header">Employee</a>
-											<div class="collapsible-body">
-												<ul>
-													<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/employee">Employee Profile</a></li>
-													<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/employeeRole">Position Roles</a></li>
-												</ul>
-											</div>
+										</ul>
 										</li>
 									</ul>
-								</li>
-								<!--Garments Dropdown-->
-								<li class="no-padding">
-									<ul class="collapsible collapsible-accordion">
-										<li>
-											<a style="color: lightcoral" class="waves-effect waves-light btn-small, collapsible-header">Garments</a>
-											<div class="collapsible-body">
-												<ul>
-													<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/garments">Category</a></li>
-													<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/garmentsDetails">Details</a></li>
-													<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/designPattern">Design Pattern</a></li>
-													<!--Measurements Dropdown-->
-													<li class="no-padding">
-														<ul class="collapsible collapsible-accordion">
-															<li>
-																<a style="color: ligthcoral" class="waves-effect waves-light btn-small, collapsible-header">Measurements</a>
-																<div class="collapsible-body">
-																	<ul>
-																		<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/measurementCategory">Header</a></li>
-																		<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/measurementDetails	">Details</a></li>
-																	</ul>
-																</div>
-															</li>
-														</ul>
-													</li>
-												</ul>
-											</div>
-										</li>
+								</div>
+							</li>
+						</ul>
+						</li>
+					<!--Materials and Fabrics Dropdown-->
+						<li class="no-padding">
+						<ul class="collapsible collapsible-accordion">
+							<li><a style="color: lightcoral" class="waves-effect waves-light btn-small, collapsible-header">Materials and Fabrics</a>
+								<div class="collapsible-body">
+									<ul>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/fabricAndMaterialsFabricType">Fabric Types</a></li>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/fabricAndMaterialsSwatches">Swatches</a></li>
+				  	<!--Materials Dropdown-->
+						<li class="no-padding">
+						<ul class="collapsible collapsible-accordion">
+							<li class="bold"><a style="color: lightcoral" class="bold, waves-effect waves-light btn-small, collapsible-header">Materials</a>
+								<div class="collapsible-body">
+									<ul>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Threads</a></li>
+								 		<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Needles</a></li>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Buttons</a></li>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Zippers</a></li>
+										<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Hook & Eye</a></li>
 									</ul>
-								</li>
-								<!--Materials and Fabrics Dropdown-->
-								<li class="no-padding">
-									<ul class="collapsible collapsible-accordion">
-										<li>
-											<a style="color: lightcoral" class="waves-effect waves-light btn-small, collapsible-header">Materials and Fabrics</a>
-											<div class="collapsible-body">
-												<ul>
-													<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/fabricAndMaterialsFabricType">Fabric Types</a></li>
-													<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/fabricAndMaterialsSwatches">Swatches</a></li>
-													<!--Materials Dropdown-->
-													<li class="no-padding">
-														<ul class="collapsible collapsible-accordion">
-															<li>
-																<a style="color: lightcoral" class="bold, waves-effect waves-light btn-small, collapsible-header">Materials</a>
-																<div class="collapsible-body">
-																	<ul>
-																		<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Threads</a></li>
-																		<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Needles</a></li>
-																		<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Buttons</a></li>
-																		<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Zippers</a></li>
-																		<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="#!">Hook & Eye</a></li>
-																	</ul>
-																</div>
-															</li>
-														</ul>
-													</li>
+								</div>
+							</li>
+						</ul>
+						</li>
 								
-												</ul>
-											</div>
-										</li>
-									</ul>
-								</li>
+						</ul>
+						</div>
+						</li>
+						</ul>
+						</li>
 								<li><a style="color: lightcoral" class="waves-effect waves-light btn-small" href="/catalogue">Catalogue</a></li>
 							</ul>
 						</div>
