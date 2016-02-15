5e49ed583495aba58bc0571dd5cbdfe95e20fb52
Marc Joseph Delim <Marc Joseph Delim>
2016-02-15 13:46:17 +0800

    modify master blade

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -134,6 +134,15 @@
 
         $('ul.tabs').tabs();
         });
+
+
+        
+  $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
+        
       </script>
 
         @yield('scripts')
